1a738b4fa2e133dff28a02b05727215d3f17c870
Adi21032004 <nonglenadi@gmail.com>
2024-10-11 08:40:33 +0530

    changes

--- src/app/supabase-server.tsx
+++ src/app/supabase-server.tsx
@@ -1,10 +1,28 @@
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 import { cache } from "react";
+import dotenv from 'dotenv'
 
-export const createServerSupabaseClient = cache(() =>
-  createServerComponentClient({ cookies })
-);
+dotenv.config({
+  path: '.env.local'
+})
+
+export const createServerSupabaseClient = cache(() => {
+  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+  const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+  console.log(supabaseKey)
+  console.log(supabaseUrl)
+
+  if (!supabaseUrl || !supabaseKey) {
+    throw new Error('Missing Supabase environment variables.');
+  }
+
+  return createServerComponentClient({
+    cookies,
+    supabaseUrl,
+    supabaseKey,
+  });
+});
 
 export async function getSession() {
   const supabase = createServerSupabaseClient();
